9e64a6d9bed7b35e5d699f57e21faeaeab9f25f5
champ <championing@mail.ru>
2021-11-20 23:45:40 +0300

    Redundant webhook removed

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::middleware('github_webhook')->group(function () {
-    Route::post('github/deploy', [GitHubWebHookController::class, 'deploy']);
-});
